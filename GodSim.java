eae145320074d281d1e38a44ac5b18d1b0bb6280
Alexander Cherry <acherry125@gmail.com>
2018-02-27 20:37:42 -0500

    Initial

---
+++ GodSim.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class GodSim extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("GodSim");
+    }
+
+    public void settings(){
+        size(300,300);
+    }
+
+    public void setup(){
+        fill(120,50,240);
+    }
+
+    public void draw(){
+        ellipse(width/2,height/2,second(),second());
+    }
+
+}

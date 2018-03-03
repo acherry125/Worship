2680d089519fdf40ce478ed26c794c60d5ec0cee
Alexander Cherry <acherry125@gmail.com>
2018-03-02 22:26:41 -0500

    Scrollable map

--- src/GodSim.java
+++ src/GodSim.java
@@ -1,20 +1,128 @@
 import processing.core.PApplet;
+import processing.event.KeyEvent;
+
+class Camera {
+    static final float SHIFT_HORIZ = 20;
+    static final float SHIFT_VERT = 20;
+    float x;
+    float y;
+    Camera() {
+        x = 0;
+        y = 0;
+    }
+    void moveLeft() {
+        x += SHIFT_HORIZ;
+    }
+    void moveRight() {
+        x -= SHIFT_HORIZ;
+    }
+    void moveUp() {
+        y += SHIFT_HORIZ;
+    }
+    void moveDown() {
+        y -= SHIFT_HORIZ;
+    }
+}
 
 public class GodSim extends PApplet {
+
+    static final float SCREEN_WIDTH = 800;
+    static final float SCREEN_HEIGHT = 800;
+
+    static final float CELLS_WIDE = 20;
+    static final float CELLS_TALL = 20;
+
+    static final float CELL_W = 100;
+    static final float CELL_H = 100;
+
+    static final float MAP_PX_WIDTH = CELL_W * CELLS_WIDE;
+    static final float MAP_PX_HEIGHT = CELL_H * CELLS_TALL;
+
+    Camera camera = new Camera();
+
+    Tile[][] board;
+
+    class Tile {
+        float temp;
+        float water;
+        Tile() {
+        }
+        Tile(float temp, float water) {
+            this.temp = temp;
+            this.water = water;
+        }
+    }
+
+    public void setup() {
+        initializeBoard();
+    }
+
+    public void settings() {
+        size((int) (SCREEN_WIDTH), (int) (SCREEN_HEIGHT));
+        //fullScreen();
+    }
+
+    void initializeBoard() {
+        // initialize the board
+        board = new Tile[(int)(CELLS_WIDE)][(int)(CELLS_TALL)];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                Tile currTile = new Tile();
+                currTile.temp =   (float) noise((float) (i * 2.5), (float) (j * 2.5)) * 100 + 50;
+                currTile.water =  (float) noise((float) (i * 1.5), (float) (j * 1.5)) * 100 + 50;
+                board[i][j] = currTile;
+            }
+        }
+    }
+
+    public void keyPressed(KeyEvent event) {
+        int keyCode = event.getKeyCode();
+        int LEFT = 37;
+        int LEFT_WASD = 65;
+        int UP = 38;
+        int UP_WASD = 87;
+        int RIGHT = 39;
+        int RIGHT_WASD = 68;
+        int DOWN = 40;
+        int DOWN_WASD = 83;
+        if (keyCode == LEFT || keyCode == LEFT_WASD) {
+            if (camera.x < 0) {
+                camera.moveLeft();
+            }
+        } else if (keyCode == UP || keyCode == UP_WASD) {
+            println(camera.y);
+            if (camera.y < 0) {
+                camera.moveUp();
+            }
+        } else if (keyCode == DOWN || keyCode == DOWN_WASD) {
+            println(camera.y);
+            if (camera.y > - (MAP_PX_HEIGHT - height)) {
+                camera.moveDown();
+            }
+        } else if (keyCode == RIGHT || keyCode == RIGHT_WASD) {
+            if (camera.x > - (MAP_PX_WIDTH - width)) {
+                camera.moveRight();
+            }
+        }
+    }
+
+    public void draw() {
+        background(255);
+        translate(camera.x, camera.y);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                Tile tile = board[i][j];
+                float x = i * CELL_W;
+                float y = j * CELL_H; //<>//
+                fill(0, tile.water, 0);
+                rect(x, y, CELL_W, CELL_H);
+                fill(0);
+                text(y, x, y + (CELL_H / 2));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         PApplet.main("GodSim");
     }
-
-    public void settings(){
-        size(300,300);
-    }
-
-    public void setup(){
-        fill(120,50,240);
-    }
-
-    public void draw(){
-        ellipse(width/2,height/2,second(),second());
-    }
-
 }
